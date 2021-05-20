e2448fc6b939cfc9d39a1e27ef6367a32c7c9ef6
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-05-20 13:09:36 +0800

    Update launch_cromwell.sh

--- launch_cromwell.sh
+++ launch_cromwell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source activate gatk4_pipeline
+conda activate gatk4_pipeline
 
 java -Dconfig.file=/data/cromwellGATK4/local.conf -jar /data/cromwellGATK4/cromwell-61.jar run /data/cromwellGATK4/Multisample_Fastq_to_Gvcf_GATK4.wdl \
    -i /data/cromwellGATK4/Multisample_Fastq_to_Gvcf_GATK4_inputs_hg38.json \
