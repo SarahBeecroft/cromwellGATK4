298cad7a732e28e90532d089129cc5ebcabf1895
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-05-20 13:09:50 +0800

    Update launch_jointgt.sh

--- launch_jointgt.sh
+++ launch_jointgt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source activate gatk4_pipeline
+conda activate gatk4_pipeline
 
 java -Dconfig.file=/data/cromwellGATK4/local.conf -jar /data/cromwellGATK4/cromwell-61.jar run -i Multisample_jointgt_GATK4_inputs_hg38.json
