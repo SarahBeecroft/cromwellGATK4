fddb16dd4d036c19b441eab1722dd64d3616ad0b
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-07-21 14:12:17 +0800

    Update launch_jointgt.sh

--- launch_jointgt.sh
+++ launch_jointgt.sh
@@ -2,4 +2,6 @@
 
 conda activate gatk4_pipeline
 
-java -Dconfig.file=/data/cromwellGATK4/local.conf -jar /data/cromwellGATK4/cromwell-61.jar run -i Multisample_jointgt_GATK4_inputs_hg38.json
+java -Dconfig.file=/data/cromwellGATK4/local.conf -jar /data/cromwellGATK4/cromwell-61.jar run Multisample_jointgt_GATK4.wdl \
+   -i Multisample_jointgt_GATK4_inputs_hg38.json \
+   -o cromwell.options
